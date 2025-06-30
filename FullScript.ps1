54cab1d7c2eb1b9e864ef6d03e9f358042eaeeca
MicrosoftSavvy <microsoftsavvy@gmail.com>
2025-06-29 21:56:08 -0400

    Update FullScript.ps1

--- FullScript.ps1
+++ FullScript.ps1
@@ -28,7 +28,6 @@ function PendingReboot {
 } 
 
 function UpdateModules {
-	
 	Install-PackageProvider -Name NuGet -Force | Out-Null
 	Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 	Repair-WinGetPackageManager
@@ -36,7 +35,6 @@ function UpdateModules {
 	Import-Module PSWindowsUpdate
 	$Winget = ((gci "C:\Program Files\WindowsApps" -Recurse -File | Where-Object { ($_.fullname -match 'C:\\Program Files\\WindowsApps\\Microsoft.DesktopAppInstaller_' -and $_.name -match 'winget.exe') } | sort fullname -descending | %{$_.FullName}) -Split [Environment]::NewLine)[0]
 	&"$Winget" source update
-
 }
 
 function Pull-Logs {
